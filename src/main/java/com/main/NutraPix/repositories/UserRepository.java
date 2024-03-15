9f1481330609ac7888472c9b391b7d058749cca0
Luizhnrs <Luizhnrsg@gmail.com>
2024-03-15 20:24:15 -0300

    Feat: find by email method, is implemented in User repository

--- src/main/java/com/main/NutraPix/repositories/UserRepository.java
+++ src/main/java/com/main/NutraPix/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.main.NutraPix.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.main.NutraPix.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
-}
+    UserDetails findByEmail(String email);
+}   
