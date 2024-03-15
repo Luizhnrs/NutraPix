8d514b8edf6ab5742a98107bc5e7829bb3cf5524
Luizhnrs <Luizhnrsg@gmail.com>
2024-03-15 19:36:09 -0300

    Initial Commit

---
+++ src/test/java/com/main/NutraPix/NutraPixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.NutraPix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutraPixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
