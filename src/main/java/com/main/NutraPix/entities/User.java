648f56600c124664f094a6fc17125aa2c1389e2b
Luizhnrs <Luizhnrsg@gmail.com>
2024-03-15 20:17:56 -0300

    Feat: User Entity Implemented

---
+++ src/main/java/com/main/NutraPix/entities/User.java
@@ -0,0 +1,79 @@
+package com.main.NutraPix.entities;
+
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+public class User implements UserDetails{
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String verificationCode;
+    private boolean enable;
+
+    public User(Long id, String name, String email, String password, String verificationCode, boolean enable) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.verificationCode = verificationCode;
+        this.enable = enable;
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enable;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+}
