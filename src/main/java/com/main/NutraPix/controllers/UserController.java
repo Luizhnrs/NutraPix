4928827c8473636e953e8009b27e58aeebcb02b0
Luizhnrs <Luizhnrsg@gmail.com>
2024-03-15 21:39:03 -0300

    Feat: Create New User Route is implemented in controller

---
+++ src/main/java/com/main/NutraPix/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.main.NutraPix.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.main.NutraPix.dtos.UserRequest;
+import com.main.NutraPix.entities.User;
+import com.main.NutraPix.services.UserService;
+
+@RestController
+@RequestMapping
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> registerUser(UserRequest userRequest){
+       User user = userRequest.toModel();
+       User userSaved = userService.registerUser(user);
+       return ResponseEntity.ok().body(userSaved);
+    }
+}
