69bc04072a1404fab39442577049ef71b73fe0df
Luizhnrs <Luizhnrsg@gmail.com>
2024-03-17 17:36:32 -0300

    Feat: @Valid annotation is implemented for Bean

--- src/main/java/com/main/NutraPix/controllers/UserController.java
+++ src/main/java/com/main/NutraPix/controllers/UserController.java
@@ -12,6 +12,8 @@ import com.main.NutraPix.dtos.UserRequest;
 import com.main.NutraPix.entities.User;
 import com.main.NutraPix.services.UserService;
 
+import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/user")
@@ -20,7 +22,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> registerUser(@RequestBody UserRequest userRequest){
+    public ResponseEntity<User> registerUser(@RequestBody @Valid UserRequest userRequest){
        User user = userRequest.toModel();
        User userSaved = userService.registerUser(user);
        return ResponseEntity.ok().body(userSaved);
